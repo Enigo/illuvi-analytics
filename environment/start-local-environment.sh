09c3b12744060bc472fac1aae1cc65131e43118b
Enigo <>
2024-11-27 00:16:05 +0100

    Migration to Postgres 17
    - also changed docker-compose to docker compose

--- environment/start-local-environment.sh
+++ environment/start-local-environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 echo "Starting environment..."
-docker-compose -p data-loader -f $SCRIPT_PATH/docker-compose.yaml up -d --build
+docker compose -p data-loader -f $SCRIPT_PATH/docker-compose.yaml up -d --build
 echo
 printf "Waiting for DB"
 while ! curl http://localhost:5432/ 2>&1 | grep '52' > /dev/null ; do
